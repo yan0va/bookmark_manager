6b9255ae39207dfbf004a6dfd971a69634f2925a
Anna <anna.yanova@gmail.com>
2014-03-26 17:14:48 +0000

    Added new links and test

--- server.rb
+++ server.rb
@@ -15,3 +15,12 @@ get '/' do
 	@links = Link.all
 	erb :index
 end
+
+post '/links' do
+	url 	= params["url"]
+	title = params["title"]
+	Link.create(:url => url, :title => title)
+	redirect to ('/')
+end
+
+

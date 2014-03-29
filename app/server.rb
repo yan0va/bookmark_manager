5d9fb1cd365d46d25d23a28aae2524b14dfbeb3e
Anna <anna.yanova@gmail.com>
2014-03-29 23:15:46 +0000

    Added user signing in function

--- app/server.rb
+++ app/server.rb
@@ -48,6 +48,20 @@ post '/users' do
 		flash.now[:errors] = @user.errors.full_messages
 	  erb :"users/new"
 	end
-
 end
 
+get '/sessions/new' do
+	erb :"sessions/new"
+end
+
+post '/sessions' do
+	email, password = params[:email], params[:password]
+	user = User.authenticate(email, password)
+	if user
+		session[:user_id] = user.id
+		redirect to('/')
+	else
+		flash[:errors] = ["The email or password are incorrect"]
+		erb :"sessions/new"
+	end
+end
